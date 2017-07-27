f7c34d8bfb74933537b6a58517b05f6d20aa7d2a
Sebastian Cotoara <sebastian.cotoara@gmail.com>
2017-07-27 15:24:53 +0300

    1

--- src/test/java/com/madison/pages/MadisonHomePage.java
+++ src/test/java/com/madison/pages/MadisonHomePage.java
@@ -1,24 +1,56 @@
 package com.madison.pages;
 
+import net.serenitybdd.core.annotations.findby.By;
 import net.serenitybdd.core.annotations.findby.FindBy;
 import net.thucydides.core.annotations.DefaultUrl;
 import net.thucydides.core.pages.PageObject;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+
 import java.util.List;
 import java.util.Random;
 
 @DefaultUrl("http://qa3.madison.com/")
 public class MadisonHomePage extends PageObject {
 
-    @FindBy(css = ".level0")
+    @FindBy(css = ".level0.parent")
+    private WebElement catMenu;
+
+    @FindBy(css = "#nav li.level0")
     private List<WebElement> categoryMenuItems;
+
+    @FindBy(css = "#nav li.level0>li.level1 a")
+    private List<WebElement> subcategoryMenu;
+
+    @FindBy(css = "a.level1")
+    private WebElement categName;
 
     public void navigateToRandomCategoryPage() {
         Random random = new Random();
-        String categories = Integer.parseInt(categoryMenuItems.get(categoryMenuItems.size()-1));
-        Actions action = new Actions(getDriver());
-        action.moveToElement();
-        System.out.println("cucubau");
+        System.out.println(categoryMenuItems.size());
+        int randomProdMenu = random.nextInt(categoryMenuItems.size() - 1);
+
+        System.out.println(categoryMenuItems.get(randomProdMenu).getText() + " " + randomProdMenu);
+
+        Actions builder = new Actions(getDriver());
+        Actions hoverOverLocationSelector = builder.moveToElement(catMenu);
+        hoverOverLocationSelector.perform();
+
+//        waitFor(categName.getText());
+
+
+
+        if (randomProdMenu > 4) {
+            categoryMenuItems.get(randomProdMenu).click();
+            System.out.println("5");
+        } else {
+            subcategoryMenu.remove(0);
+            System.out.println(subcategoryMenu.size());
+            int randomSubcategory = random.nextInt(subcategoryMenu.size()-1);
+            System.out.println("submenu: " + subcategoryMenu.get(randomSubcategory).getText() + randomSubcategory);
+            Actions action = new Actions(getDriver());
+            action.moveToElement(categoryMenuItems.get(randomProdMenu)).moveToElement(subcategoryMenu.get(randomSubcategory));
+        }
+
     }
 }
